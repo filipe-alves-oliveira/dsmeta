32306a28b554c38265eacddb07486c3520e259f4
ROL - Rendimento Online <filipe_alvesoliveira@hotmail.com>
2022-09-16 20:14:59 -0300

    start integration on backend

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -6,10 +6,10 @@ function Header() {
     <header>
       <div className="dsmeta-logo-container">
         <img src={logo} alt="DSMeta" />
-        <h1>DSMeta</h1>
+        <h1>DSMeta - FAO</h1>
         <p>
           Desenvolvido por
-          <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+          <a href="https://github.com/filipe-alves-oliveira"> Filipe Alves</a>
         </p>
       </div>
     </header>
