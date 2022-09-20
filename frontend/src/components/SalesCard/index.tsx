588461df021594df9d9385c9aedf5b206283561b
ROL - Rendimento Online <filipe_alvesoliveira@hotmail.com>
2022-09-19 21:41:20 -0300

    Sale listining

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,22 +2,24 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
 import NotificationButton from "../NotificationButton";
 import "./styles.css";
 
 function SalesCard() {
-  
   const min = new Date(new Date().setDate(new Date().getDate() - 365));
   const max = new Date();
 
   const [minDate, setMinDate] = useState(min); //declaraçao de um estado dentro de um componente React.
   const [maxDate, setMaxDate] = useState(max);
 
+  const [sales, setsales] = useState<sale[]>([]);
+
   useEffect(() => {
-    axios.get("http://localhost:8080/sales")
-      .then(response => {
-        console.log(response.data);
-      })
+    axios.get(`${BASE_URL}/sales`).then((response) => {
+      setsales(response.data.content);
+    });
   }, []);
 
   return (
@@ -56,45 +58,24 @@ function SalesCard() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
+            {sales.map((sale) => {
+              return ( //qdo faz uma renderizaçao de conteudo baseado em uma lista tem que colocar a palavra key
+              //passando um valor unico para key.
+                <tr key={sale.id}>  
+                  <td className="show992">{sale.id}</td>
+                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                  <td>{sale.sellerName}</td>
+                  <td className="show992">{sale.visited}</td>
+                  <td className="show992">{sale.visited}</td>
+                  <td>{sale.amount.toFixed(2)}</td>
+                  <td>
+                    <div className="dsmeta-red-btn-container">
+                      <NotificationButton />
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080"; //?? operador de coalescência nula 
+
+//macete para que o frontend pegue valor local caso nao tenha instalado o frontend ainda e definido as variaveis de ambiente
